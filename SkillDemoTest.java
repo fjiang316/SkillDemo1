65406ba5277698595f4e4029d9e06975aae887d2
fjiang316 <fejiang@ucsd.edu>
2022-05-01 10:42:32 -0700

    Update SkillDemoTest.java

--- SkillDemoTest.java
+++ SkillDemoTest.java
@@ -2,7 +2,7 @@ import static org.junit.Assert.*;
 import org.junit.*;
 
 public class SkillDemoTest {
-    @test
+    @Test
 	public void testSumIsOdd() {
 		Integer[] arr = {8, 16, 28};
 		assertTrue("The sum is odd", SkillDemo.sumIsOdd(arr));
